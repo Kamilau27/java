a647872bed7d3ef67de654ba86acfcd7b048ccd6
Kamilau27 <kamilau27@hotmail.com>
2024-01-04 13:28:14 -0500

    git commit -m "avance"

---
+++ java.js
@@ -0,0 +1,9 @@
+"use strict"
+// Preguntar nombre y responder hola, si no responder hola desconocido 
+
+const nombre = prompt("Por favor ingrese su nombre")
+if (nombre) {
+    document.write("Hola," = nombre);
+} else {
+    document.write("Hola, desconocido");
+}
