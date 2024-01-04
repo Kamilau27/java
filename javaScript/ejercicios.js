3852a6a1a6bf7e257ede7c02aa5392a52a8b5c9b
Kamilau27 <kamilau27@hotmail.com>
2024-01-04 13:44:46 -0500

    git commit -m "finis"

--- javaScript/ejercicios.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-// Preguntar nombre y responder hola, si no responder hola desconocido 
-
-const nombre = prompt("Por favor ingrese su nombre")
-if (nombre) {
-    document.write("Hola," = nombre);
-} else {
-    document.write("Hola, desconocido");
-}
